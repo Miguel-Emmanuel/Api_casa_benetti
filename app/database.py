dc2b37cb763158a03428455d1933de235d5b12d6
MIKE <al222010230@gmail.com>
2025-06-02 18:05:19 -0600

    Correccion de requerimientos

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Cambia los valores de usuario, contraseña, host y base de datos según tu configuración de MySQL
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://benetti_user:TuPasswordSegura123@localhost/pagos_benetti"
+# Lee la URL de la base de datos desde la variable de entorno DATABASE_URL (para Render)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
